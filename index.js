8a0aa9f0db1996982c9f9b72511ea6e23bb003fb
abzakcd <abzakcd@gmail.com>
2023-09-28 01:08:45 +0300

    add data

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = 7575;
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "js")));
+
+var db_M = require('./database');
+global.db_pool = db_M.pool;
+
