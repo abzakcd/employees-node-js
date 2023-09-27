aaa5b719cfbe9f6124d309744f41bbe9431a15ae
abzakcd <abzakcd@gmail.com>
2023-09-28 01:22:16 +0300

    employees add

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// npm i express mysql2 ejs body-parser
 const express = require('express');
 const port = 7575;
 const app = express();
@@ -14,4 +15,18 @@ app.use(express.static(path.join(__dirname, "js")));
 
 var db_M = require('./database');
 global.db_pool = db_M.pool;
+
+//yanal
+const emp_rtr = require('./routes/employees_R');
+app.use('/', emp_rtr);
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
+
+
+
+
+
 
